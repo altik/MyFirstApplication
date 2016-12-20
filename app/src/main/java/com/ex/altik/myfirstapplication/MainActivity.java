c0544d2921e7017b6f47828654d66a3ad7387397
altik <altikk@gmail.com>
2016-12-20 17:09:57 +0200

    Initial commit

---
+++ app/src/main/java/com/ex/altik/myfirstapplication/MainActivity.java
@@ -0,0 +1,16 @@
+package com.ex.altik.myfirstapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
