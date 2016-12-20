c42eff74789a63bd2ac3e5348cac1c98fe84fc09
altik <altikk@gmail.com>
2016-12-20 17:13:43 +0200

    first

--- app/src/main/java/com/ex/altik/myfirstapplication/MainActivity.java
+++ app/src/main/java/com/ex/altik/myfirstapplication/MainActivity.java
@@ -1,9 +1,7 @@
 package com.ex.altik.myfirstapplication;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.LinearLayout;
-import android.widget.RelativeLayout;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
